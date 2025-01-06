0d73929c2a6b081ffb68f08eead52cd5bde973dc
Francelino <fmarcilio1989@gmail.com>
2025-01-06 12:47:06 -0300

    Feat: exemplo de como utilizar o useRef com o DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useRef, useState} from "react";
+import RefExemple from "./RefExemple.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    let variable = 0;
+    const [state, setState] = useState(0);
+    const ref = useRef(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const showVariabel = () => {
+        alert(
+            `
+            Variable: ${variable}
+            Ref: ${ref.current}
+            State: ${state}
+            `
+        )
+    }
+
+    return (
+        <div>
+            <h2>Conhecendo o useRef</h2>
+            <p>Variable: {variable}</p>
+            <p>Ref: {ref.current}</p>
+            <p>State: {state}</p>
+
+            <button onClick={() => variable++}>Aumentar Variavel</button>
+            <button onClick={() => ref.current++}>Aumentar useRef</button>
+            <button onClick={() => setState(state + 1)}>Aumentar State</button>
+            <hr />
+            <button onClick={showVariabel}>Mostrar Valores</button>
+
+            <hr />
+            <h2>Exemplo de useRef com o DOM</h2>
+            <RefExemple />
+        </div>
+    )
 }
 
 export default App
