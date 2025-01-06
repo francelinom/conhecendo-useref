10832e07cb12ed6b52d852aedd7254d4859ab6ae
Francelino <fmarcilio1989@gmail.com>
2025-01-06 14:37:40 -0300

    Feat: exemplo de como utilizar o useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import RefExemple from "./RefExemple.jsx";
+
+function Couter() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('Iniciando component');
+        return () => {
+            console.log('Limpando component');
+            console.log('Finalizando component');
+        }
+    }, []);
+
+    return (
+        <div>
+            <h2>Contador</h2>
+            <p>{count}</p>
+            <button onClick={() => setCount(count + 1)}>Aumentar</button>
+        </div>
+    )
+}
 
 function App() {
     let variable = 0;
     const [state, setState] = useState(0);
     const ref = useRef(0);
+
+    const [show, setShow] = useState(false);
 
     const showVariabel = () => {
         alert(
@@ -26,12 +48,20 @@ function App() {
             <button onClick={() => variable++}>Aumentar Variavel</button>
             <button onClick={() => ref.current++}>Aumentar useRef</button>
             <button onClick={() => setState(state + 1)}>Aumentar State</button>
-            <hr />
+            <hr/>
             <button onClick={showVariabel}>Mostrar Valores</button>
 
-            <hr />
+            <hr/>
             <h2>Exemplo de useRef com o DOM</h2>
-            <RefExemple />
+            <RefExemple/>
+
+            <br/>
+            <hr/>
+
+            <h2>Exemplo de useEffect</h2>
+            <label htmlFor="show">Mostrar contador</label>
+            <input type="checkbox" id="show" value={show} onChange={() => setShow(state => !state)} />
+            {show ? <Couter /> : null}
         </div>
     )
 }
